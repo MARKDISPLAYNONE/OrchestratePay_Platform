d4324e7757a66ea7be29f3c07befc0a26e33b5c6
Gabriel <gabriel.ngige@student.moringaschool.com>
2026-06-20 23:20:28 +0300

    upgrade/next.js14-15

--- Tap2Pay/web/.next/server/server-reference-manifest.js
+++ Tap2Pay/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"p0oaKBzhh3DAFe1FupRDqPAdTThOeeTk7q2Be2ndqtY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
